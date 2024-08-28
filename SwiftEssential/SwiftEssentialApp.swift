fb41ab8fe8e37034779ebb9a36acc4a709c09ad9
dave17code <dave17code@gmail.com>
2024-08-28 09:19:41 +0900

    Initial Commit

---
+++ SwiftEssential/SwiftEssentialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftEssentialApp.swift
+//  SwiftEssential
+//
+//  Created by 김두운 on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftEssentialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
