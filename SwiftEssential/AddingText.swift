ec4d2304ebdb7aefb1156fa829c04fe2099a51f3
dave17code <dave17code@gmail.com>
2024-08-28 09:47:34 +0900

    SwiftUI-Essential

---
+++ SwiftEssential/AddingText.swift
@@ -0,0 +1,18 @@
+//
+//  AddingText.swift
+//  SwiftEssential
+//
+//  Created by 김두운 on 8/28/24.
+//
+
+import SwiftUI
+
+struct AddingText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddingText()
+}
