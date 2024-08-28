47accbe482467b2c496f44b43ec791efa3bbdb89
dave17code <dave17code@gmail.com>
2024-08-28 10:12:16 +0900

    AddingText

--- SwiftEssential/AddingText.swift
+++ SwiftEssential/AddingText.swift
@@ -10,6 +10,13 @@ import SwiftUI
 struct AddingText: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//            .font(.title)
+//            .fontWeight(.semibold)
+            .underline(true, color: Color.yellow)
+            .strikethrough(true, color: Color.red)
+            .baselineOffset(10)
+            .kerning(0.8)
+            .font(.system(size: 60, weight: .semibold))
     }
 }
 
