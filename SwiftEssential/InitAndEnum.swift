6ce3b520313dc0ad346bf4b79b94ec2674f129fc
dave17code <dave17code@gmail.com>
2024-08-29 19:55:43 +0900

    InitAndEnum

---
+++ SwiftEssential/InitAndEnum.swift
@@ -0,0 +1,57 @@
+//
+//  InitAndEnum.swift
+//  SwiftEssential
+//
+//  Created by 김두운 on 8/29/24.
+//
+
+import SwiftUI
+
+struct InitAndEnum: View {
+    let backgroundColor: Color
+    let country: String
+    let title: String
+    
+//    init(backgroundColor: Color, country: String, title: String) {
+//        self.backgroundColor = backgroundColor
+//        self.country = country
+//        self.title = title
+//    }
+    
+    init(country: String, brand: CarBrand) {
+        self.country = country
+        if country == "Korea" {
+            self.title = "Hyundai"
+            self.backgroundColor = Color.yellow
+        } else {
+            self.title = "Tesla"
+            self.backgroundColor = Color.pink
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Text("\(country)")
+                .font(.system(size: 20, weight: .semibold))
+            Text("\(title)")
+                .font(.system(size: 25, weight: .bold))
+        }
+        .foregroundStyle(Color.white)
+        .frame(width: 120, height: 120)
+        .background(backgroundColor)
+        .clipShape(Circle())
+    }
+    
+    //enum
+    enum CarBrand {
+        case Hyundai
+        case Tesla
+    }
+}
+
+#Preview {
+    HStack {
+        InitAndEnum(country: "Korea", brand: .Hyundai)
+        InitAndEnum(country: "US", brand: .Tesla)
+    }
+}
