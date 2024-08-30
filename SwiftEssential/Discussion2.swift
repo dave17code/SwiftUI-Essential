d478db322264c0ccff0342b4033d6abe268df213
dave17code <dave17code@gmail.com>
2024-08-30 11:51:07 +0900

    Discussion2

---
+++ SwiftEssential/Discussion2.swift
@@ -0,0 +1,20 @@
+//
+//  Discussion2.swift
+//  SwiftEssential
+//
+//  Created by 김두운 on 8/30/24.
+//
+
+import SwiftUI
+
+struct Discussion2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.yellow, ignoresSafeAreaEdges: [])
+    }
+}
+
+#Preview {
+    Discussion2()
+}
