b15cd77fc9a1e4114ed89db6716c82d348ed84e3
dave17code <dave17code@gmail.com>
2024-09-02 22:07:31 +0900

    ListLayout

---
+++ SwiftEssential/ListLayout.swift
@@ -0,0 +1,44 @@
+//
+//  ListLayout.swift
+//  SwiftEssential
+//
+//  Created by 김두운 on 9/2/24.
+//
+
+import SwiftUI
+
+struct ListLayout: View {
+    
+    @State var showSectionArrow = true
+    @State var cars = [
+        "Hyundai",
+        "KIA",
+        "Tesla",
+        "BMW",
+        "Mercesdes"
+    ]
+    
+    var body: some View {
+        List {
+            Section("Car Brands", isExpanded: $showSectionArrow)  {
+                ForEach(cars, id: \.self) { car in
+                    Text("\(car)")
+                }
+                .onDelete(perform: { indexSet in
+                    cars.remove(atOffsets: indexSet)
+                })
+            }
+            Section("Detail Model") {
+                Text("GV80")
+                Text("K5")
+                Text("BMW 520")
+                Text("E Class")
+            }
+        }
+        .listStyle(.sidebar)
+    }
+}
+
+#Preview {
+    ListLayout()
+}
